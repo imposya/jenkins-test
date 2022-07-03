93574cf10c083dcd79f7f8f94bb6cf1dc2558da2
Alexander Latyshev <imposya0@gmail.com>
2022-07-03 18:57:11 +0300

    @RequestParam Simple calculator

--- src/main/java/ru/imposya/springmvc/controllers/FirstController.java
+++ src/main/java/ru/imposya/springmvc/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.imposya.springmvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,13 +12,49 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello, " + name + " " + surname);
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+
+        //System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodbyePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculatorPage(@RequestParam(value = "a") int a,
+                                 @RequestParam(value = "b") int b,
+                                 @RequestParam(value = "action") String action,
+    Model model)
+
+    {
+        double result;
+        switch(action) {
+            case ("multiplication") :
+                result = a * b;
+                model.addAttribute("message", "a * b = " + result);
+                break;
+            case ("addition") :
+                result = a + b;
+                model.addAttribute("message", "a + b = " + result);
+                break;
+            case ("substraction") :
+                result = a - b;
+                model.addAttribute("message", "a - b = " + result);
+                break;
+            case ("division") :
+                result = (double) a / b;
+                model.addAttribute("message", "a / b = " + result);
+                break;
+            default:
+                model.addAttribute("message", "wrong action");
+                break;
+        }
+        return "first/calculator";
     }
 }
